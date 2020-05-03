c70068565dfd490f855eeb3da627c97d0159f615
xofbd <dfox09@gmail.com>
2020-05-03 18:21:20 -0400

    add combine_jsons.py

---
+++ combine_jsons.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+import glob
+import json
+import os
+import re
+
+
+def get_unique_dates(files):
+    """Return a set of all unique dates from all JSON files."""
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    past_files = [f for f in files if current_date not in f]
+
+    re_date = re.compile(r'(\d{4}-\d{2}-\d{2})')
+    unique_dates = {re_date.search(f).group(1) for f in past_files}
+
+    return unique_dates
+
+
+def combine_jsons(files, date):
+    """Create JSON file from all JSONs of the same date."""
+
+    # Load up all JSONs of the same date into one Python list
+    data_all = []
+    files_of_date = [f for f in files if date in f]
+
+    for f in files_of_date:
+        with open(f, 'r') as f:
+            data_all.extend(json.load(f))
+
+    # Dump collected JSON to disk
+    try:
+        os.mkdir(local_path('daily'))
+    except OSError:
+        pass
+
+    file_name = 'venmo_data' + '_' + date + '.json'
+    path_name = os.path.join(local_path('daily'), file_name)
+    print("Dumping data to {}".format(path_name))
+
+    with open(path_name, 'w') as f:
+        json.dump(data_all, f)
+
+    # Delete snapshot files
+    [os.remove(f) for f in files_of_date]
+
+
+def main():
+    files = glob.glob(os.path.join(
+        os.path.dirname(__file__), 'data', '*.json'))
+    unique_dates = get_unique_dates(files)
+
+    for date in unique_dates:
+        combine_jsons(files, date)
+
+
+def local_path(path):
+    """Return path relative to local file."""
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+if __name__ == '__main__':
+    main()
