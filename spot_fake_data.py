a7293c61cd72555b06ac692cef692fb3891ef01e
xofbd <dfox09@gmail.com>
2020-05-03 18:21:04 -0400

    add spot_fake_data.py

---
+++ spot_fake_data.py
@@ -0,0 +1,38 @@
+"""
+This script looks to spot when Venmo started releasing fake data to their Public
+API.
+"""
+import glob
+import json
+
+
+def load_data(path):
+    """Return decoded JSON."""
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+def name_counter(data):
+    """Return the number of unique names."""
+    names = [x['actor']['firstname'].lower() for x in data]
+
+    return len(set(names))
+
+
+def spot_fakes(threshold=20):
+    fake_files = []
+
+    for f in glob.glob('daily/*.json'):
+        data = load_data(f)
+        num_names = name_counter(data)
+
+        if num_names < threshold:
+            fake_files.append(f)
+
+    fake_files.sort()
+
+    print(f"Earliest file deemed fake: {fake_files[0]}")
+
+
+if __name__ == '__main__':
+    spot_fakes()
